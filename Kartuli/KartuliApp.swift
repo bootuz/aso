53156e04e1f0ac6e5193ba4b3ecd802df8d69012
Astemir Boziev <bootuz07@gmail.com>
2023-01-14 20:49:12 +0400

    Initial Commit

---
+++ Kartuli/KartuliApp.swift
@@ -0,0 +1,17 @@
+//
+//  KartuliApp.swift
+//  Kartuli
+//
+//  Created by Астемир Бозиев on 14.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct KartuliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
